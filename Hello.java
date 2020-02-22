c84c8996a02609d889ffa1453552caa8af37dcb0
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-02-22 13:21:14 -0600

    messing variables.

--- Hello.java
+++ Hello.java
@@ -2,5 +2,16 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello John");
+
+        // int myFirstNumber = 5;
+        int myFirstNumber = (5 + 10) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
+
     }
+
 }
