002661ca5beeec3a3134945eda616b487865a15f
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-02-23 07:09:43 -0600

    finished basics

---
+++ KeywordsAndExpressions/Main.java
@@ -0,0 +1,7 @@
+package KeywordsAndExpressions;
+
+class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- Operators/Main.java
+++ Operators/Main.java
@@ -47,12 +47,54 @@ public class Main {
         }
 
         int topScore = 100;
+        int secondTopScore = 95;
+        int newValue = 50;
+        boolean isCar = false;
 
         // < > <= >= == != #comparison ops
         // && || #and or
 
-        if (topScore != 100) {
+        if (topScore < 100) {
             System.out.println("You got the high score!");
+        }
+
+        if ((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        if (newValue == 50) {
+            System.out.println("This is true");
+        }
+
+        if (isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
+
+        // operator Challenge
+
+        double a = 20.00d;
+        double b = 80.00d;
+        double c = (a + b) * 100.00d;
+        double d = c % 40.00d;
+        boolean e;
+
+        if (d == 0.00) {
+            e = true;
+            System.out.println("e is equal to zero");
+        } else {
+            e = false;
+            System.out.println("Got some remainder " + e);
         }
 
     }
