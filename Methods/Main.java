cb5f7b1be15e905272e5f08a181cfdb6cbed3302
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-02-25 20:36:22 -0600

    finished methods with logic hole fixes

--- Methods/Main.java
+++ Methods/Main.java
@@ -4,17 +4,50 @@ public class Main {
 
     public static void main(String[] args) {
 
-        caculateScore(true, 800, 5, 100);
-        caculateScore(true, 10000, 8, 200);
+        int highScore = caculateScore(true, 800, 5, 100);
+        System.out.println("Your final score was " + highScore);
+
+        highScore = caculateScore(true, 10000, 8, 200);
+        System.out.println("Your final score was " + highScore);
+
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Tim", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Bob", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Bill", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Rick", highScorePosition);
 
     }
 
-    public static void caculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int caculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
+            return finalScore;
         }
+
+        return -1;
+
+    }
+
+    public static void displayHighScorePosition(String playersName, int position) {
+        System.out.println(playersName + " managed to get into position " + position + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500) {
+            return 2;
+        } else if (playerScore >= 100) {
+            return 3;
+        }
+        return 4;
     }
 }
