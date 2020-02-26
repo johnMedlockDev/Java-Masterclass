8b6930efa8ffa553820fe8f90a9188b8a7ca8b7c
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-02-25 20:01:53 -0600

    refactored to methods

--- Methods/Main.java
+++ Methods/Main.java
@@ -3,22 +3,17 @@ package Methods;
 public class Main {
 
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+
+        caculateScore(true, 800, 5, 100);
+        caculateScore(true, 10000, 8, 200);
+
+    }
+
+    public static void caculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
-        }
-
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
             System.out.println("Your final score was " + finalScore);
         }
     }
