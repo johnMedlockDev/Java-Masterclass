1d4085932fc43e89cb7efad68b40dfbfcd99d44e
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-02-27 19:55:00 -0600

    Create MegaBytesConverter.java

---
+++ MegaBytesConverter/MegaBytesConverter.java
@@ -0,0 +1,62 @@
+package MegaBytesConverter;
+
+public class MegaBytesConverter {
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+
+        int megaByte = 1024;
+
+        int MB = kiloBytes / megaByte;
+        int KB = kiloBytes % megaByte;
+
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kiloBytes + " KB = " + MB + " MB and " + KB + " KB");
+        }
+    }
+
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(-1024);
+    }
+
+}
+
+// The method should not return anything (void) and it needs to calculate the
+// megabytes and remaining kilobytes from the kilobytes parameter.
+
+// Then it needs to print a message in the format "XX KB = YY MB and ZZ KB".
+
+// XX represents the original value kiloBytes.
+// YY represents the calculated megabytes.
+// ZZ represents the calculated remaining kilobytes.
+
+// For example, when the parameter kiloBytes is 2500 it needs to print "2500 KB
+// = 2 MB and 452 KB"
+
+// If the parameter kiloBytes is less than 0 then print the text "Invalid
+// Value".
+
+// EXAMPLE INPUT/OUTPUT
+
+// * printMegaBytesAndKiloBytes(2500); → should print the following text: "2500
+// KB = 2 MB and 452 KB"
+
+// * printMegaBytesAndKiloBytes(-1024); → should print the following text:
+// "Invalid Value" because parameter is less than 0.
+
+// * printMegaBytesAndKiloBytes(5000); → should print the following text: "5000
+// KB = 4 MB and 904 KB"
+
+// TIP: Be extremely careful about spaces in the printed message.
+
+// TIP: Use the remainder operator
+
+// TIP: 1 MB = 1024 KB
+
+// NOTE: Do not set kilobytes parameter value inside your method.
+
+// NOTE: The solution will not be accepted if there are extra spaces.
+// NOTE: The printMegaBytesAndKiloBytes method needs to be defined as public
+// static like we have been doing so far in the course.NOTE: Do not add a main
+// method to solution code.
