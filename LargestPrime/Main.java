69462f69c6b10be4a1668108438be7f9eb3c56c8
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-03-02 23:22:10 -0600

    Largest Prime solution

---
+++ LargestPrime/Main.java
@@ -0,0 +1,82 @@
+package LargestPrime;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(21));
+        System.out.println(getLargestPrime(217));
+        System.out.println(getLargestPrime(0));
+        System.out.println(getLargestPrime(45));
+        System.out.println(getLargestPrime(-1));
+        System.out.println(getLargestPrime(7));
+    }
+
+    public static int getLargestPrime(int number) {
+
+        if (number >= 0) {
+            int largestPrime = 0;
+            int primeCounter = 0;
+
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    int factor = i;
+                    int count = 0;
+
+                    for (int j = 1; j <= factor; j++) {
+                        if (factor % j == 0) {
+                            count++;
+                        }
+                    }
+                    if (count == 2) {
+                        primeCounter++;
+                        int prime = factor; // getting prime number
+
+                        if (prime > largestPrime) {
+                            largestPrime = prime; // checking that prime number is largest one?
+                        }
+                    }
+                }
+            }
+            if (primeCounter == 0) {
+                return -1;
+            }
+            return largestPrime;
+        }
+        return -1;
+    }
+}
+
+// The method should calculate the largest prime factor of a given number and
+// return it.
+
+// EXAMPLE INPUT/OUTPUT:
+
+// * getLargestPrime (21); should return 7 since 7 is the largest prime (3 * 7 =
+// 21)
+
+// * getLargestPrime (217); should return 31 since 31 is the largest prime (7 *
+// 31 = 217)
+
+// * getLargestPrime (0); should return -1 since 0 does not have any prime
+// numbers
+
+// * getLargestPrime (45); should return 5 since 5 is the largest prime (3 * 3 *
+// 5 = 45)
+
+// * getLargestPrime (-1); should return -1 since the parameter is negative
+
+// HINT: Since the numbers 0 and 1 are not considered prime numbers, they cannot
+// contain prime numbers.
+
+// ---------------------------------------------------------------------------
+
+// NOTE: The method getLargestPrime should be defined as public static like we
+// have been doing so far in the course.
+
+// NOTE: Do not add a main method to the solution code.
+
+// Write a method named getLargestPrime with one parameter of type int named
+// number.
+
+// If the number is negative or does not have any prime numbers, the method
+// should return -1 to indicate an invalid value.
