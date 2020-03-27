a23f5b748300516f1d170f71ad9744af0405043e
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-03-27 10:46:55 -0500

    PolymorphismChallenge

---
+++ PolymorphismChallenge/Main.java
@@ -0,0 +1,21 @@
+package PolymorphismChallenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car car = new Car(8, "Base Car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRW 4WD");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+        Ford ford = new Ford(6, "Falcon");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+    }
+}
